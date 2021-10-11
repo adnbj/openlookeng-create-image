f8fd7cde5651cb1db79bb096ebb4b771eb11706e
adnbj <78532744+adnbj@users.noreply.github.com>
2021-10-11 10:40:01 +0800

    Add files via upload

---
+++ file/variables.tf
@@ -0,0 +1,27 @@
+variable "region"{
+  type = string
+  
+}
+variable "ak"{
+  type = string
+  
+}
+variable "sk"{
+  type = string
+  
+}
+
+variable "privateUrl"{
+  type = string
+}
+
+variable "publicUrl"{
+  type = string
+  
+}
+
+
+variable "url"{
+  type = string
+  
+}
